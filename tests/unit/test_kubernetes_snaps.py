ea6ff4564c185accd6c92f455d03826e91f6fad8
Adam Dyess <adam.dyess@canonical.com>
2023-11-03 18:15:49 -0500

    lint

--- tests/unit/test_kubernetes_snaps.py
+++ tests/unit/test_kubernetes_snaps.py
@@ -1,11 +1,6 @@
-from dataclasses import dataclass
-from ops.framework import Framework
-import os
 import pytest
 import unittest.mock as mock
 
-import ops
-import ops.testing
 
 from charms import kubernetes_snaps
 
@@ -16,11 +11,16 @@ def subprocess_check_output():
         yield mock_run
 
 
-
 def test_upgrade_action_control_plane(caplog):
     mock_event = mock.MagicMock()
-    with mock.patch.object(kubernetes_snaps, 'is_upgrade', return_value=False):
-        with mock.patch.object(kubernetes_snaps, 'install_snap'):
+    with mock.patch.object(kubernetes_snaps, "is_upgrade", return_value=False):
+        with mock.patch.object(kubernetes_snaps, "install_snap"):
             kubernetes_snaps.upgrade_snaps("1.28/edge", mock_event, control_plane=True)
-    assert "Starting the upgrade of Kubernetes snaps to '1.28/edge' channel." in caplog.messages
-    assert "Successfully upgraded Kubernetes snaps to the '1.28/edge' channel." in caplog.messages
+    assert (
+        "Starting the upgrade of Kubernetes snaps to '1.28/edge' channel."
+        in caplog.messages
+    )
+    assert (
+        "Successfully upgraded Kubernetes snaps to the '1.28/edge' channel."
+        in caplog.messages
+    )
